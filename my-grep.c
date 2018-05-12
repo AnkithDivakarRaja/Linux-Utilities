6130b6f008118e3704f1bcfd6dda0c53b2683ec2
AnkithRaja <araja2@ncsu.edu>
2018-05-11 21:01:09 -0700

    Match with stdin

--- my-grep.c
+++ my-grep.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 int main(int argc, char *argv[]){
 
-  if(argc < 3){
+  if(argc < 2){
     printf("my-grep: searchterm [file ...]\n");
     exit(1);
   }
@@ -13,24 +14,36 @@ int main(int argc, char *argv[]){
   char *buf;
   size_t bufsize = 32;
 
-  for(i=2; i<argc; i++ ){
+  if(argc > 2){
+    for(i=2; i<argc; i++ ){
 
-      char *str = argv[i];
+        char *str = argv[i];
 
-      FILE *fp = fopen(str, "r");
-      if(fp == NULL){
-        printf("my-grep: cannot open file\n");
-        exit(1);
-      }
-
-      buf = (char *)malloc(bufsize * sizeof(char));
-      while(getline(&buf, &bufsize, fp) != -1)
-      {
-        if(strstr(buf, argv[1]) != NULL){
-            printf("%s\n", buf);
+        FILE *fp = fopen(str, "r");
+        if(fp == NULL){
+          printf("my-grep: cannot open file\n");
+          exit(1);
         }
-      }
+
+        buf = (char *)malloc(bufsize * sizeof(char));
+        while(getline(&buf, &bufsize, fp) != -1)
+        {
+          if(strstr(buf, argv[1]) != NULL){
+              printf("%s\n", buf);
+          }
+        }
+    }
   }
+  else
+  {
+    char line[LINE_MAX];
+    if(fgets(line, LINE_MAX, stdin) != NULL){
+      if(strstr(line, argv[1]) != NULL){
+          printf("%s\n", line);
+      }
+    }
+  }
+
 
   exit(1);
 
